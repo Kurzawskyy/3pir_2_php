aa16a276873b5975ae0de73eddad1ffc69c4df07
Kurzawskyy <kurzawski.n@gmail.com>
2023-03-29 12:26:38 +0200

    zajecia 29.03.23 // t0x // skonczone wyslane

--- t07e_tab_znakow_slowa/index.php
+++ t07e_tab_znakow_slowa/index.php
@@ -20,10 +20,18 @@
     <input type="text" name="c7"> <br><br>
     <input type="text" name="c8"> <br><br>
     <input type="text" name="c9"> <br><br>
-    <input type="submit" value="Wyślij">
+
+    <h3>Podaj ile słow należy wygenerować i ile mają mieć liter</h3>
+    <label for='ile_slow'>Ile słów: </label>
+    <input type='text' name='ile_slow'> <br><br>
+    <label for='ile_liter'>Ile liter: </label>
+    <input type='text' name='ile_liter'> <br><br>
+
+    <input type="submit" value="Wyślij"> <br><br>
 </form>
 
 <?php
+// Norbert Kurzawski 3pir02
     if(
             isset($_POST["c0"]) &&
             isset($_POST["c1"]) &&
@@ -34,16 +42,36 @@
             isset($_POST["c6"]) &&
             isset($_POST["c7"]) &&
             isset($_POST["c8"]) &&
-            isset($_POST["c9"])
+            isset($_POST["c9"]) &&
+            isset($_POST['ile_liter']) &&
+            isset($_POST['ile_slow'])
 ) {
         $tab = array();
         for($i = 0; $i < 10; $i++) {
             $tab[$i] = $_POST["c".$i];
         }
 
+        for($i = 0; $i < 10; $i++) {
+            echo $tab[$i]." ";
+        }
 
-    }
+        echo "<br><br>";
 
+            $slow = $_POST['ile_slow'];
+            $liter = $_POST['ile_liter'];
+
+            $s = "";
+            for ($j = 0; $j < $slow; $j++) {
+                $s = "";
+                for ($i = 0; $i < $liter; $i++) {
+
+                    $x = rand(0, 9);
+                    $s = $s . $tab[$x];
+
+                }
+                echo "$s<br>";
+            }
+        }
 ?>
 </body>
 </html>
